29d68c4e5c90f10910a0909c786e0194ac69f848
Jun-Honda-0326 <j.honda.0326@gmail.com>
2022-11-26 00:46:26 +0900

    create db parameter group

--- rds.tf
+++ rds.tf
@@ -1,1 +1,28 @@
 # DB サブネットグループの作成
+resource "aws_db_subnet_group" "db-subnet" {
+  name = "web-rds-db-subnet"
+  description = "Web-RDS-DB-Subnet"
+  subnet_ids = [
+    aws_subnet.web-site-private_1a.id,
+    aws_subnet.web-site-private_1c.id
+  ]
+  tags = {
+    Name = "web-rds-db-subnet"
+  }
+}
+
+# DBパラメータグループ
+resource "aws_db_parameter_group" "mysql-parameter-group" {
+  name = "rds-pg"
+  family = "mysql8.0"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8"
+  }
+
+  parameter {
+    name  = "character_set_client"
+    value = "utf8"
+  }
+}
